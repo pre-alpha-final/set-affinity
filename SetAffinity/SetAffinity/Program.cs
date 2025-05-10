a4c8bcff8052aca1ab6f56f6b1996ae1771690fc
pre-alpha-final <jacek.mowinski@gmail.com>
2025-05-10 17:15:08 +0200

    Initial commit

---
+++ SetAffinity/SetAffinity/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace SetAffinity;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var programStartTime = DateTime.Now;
+        while (true)
+        {
+            var processes = Process.GetProcesses();
+            _ = Task.Run(async () =>
+            {
+                await Task.Delay(20000);
+                foreach (var process in processes)
+                {
+                    try
+                    {
+                        if (process.StartTime > programStartTime)
+                        {
+                            process.ProcessorAffinity = (nint)4294966527;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        // ignore
+                    }
+                }
+            });
+            await Task.Delay(5000);
+        }
+    }
+}
