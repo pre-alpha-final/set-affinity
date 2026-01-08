7aa16c4d09a8ed1465f01796b542e1fcfa7e8715
pre-alpha-final <jacek.mowinski@gmail.com>
2026-01-08 19:06:21 +0100

    Blacklist app launchers to prevent auto inheritance of affinity

--- src/SetAffinity/App.cs
+++ src/SetAffinity/App.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.Extensions.Hosting;
 using System.Diagnostics;
+using System.Management;
 
 namespace SetAffinity;
 
 internal class App : BackgroundService
 {
     private readonly string[] _args;
+    private readonly List<string> _appLaunchersBlacklist =
+    [
+        "explorer.exe",
+        "svchost.exe -k netsvcs -p -s Schedule",
+        "taskhostw.exe",
+        //"winlogon.exe",
+        //"userinit.exe",
+        //"wininit.exe",
+        //"services.exe",
+        //"lsass.exe",
+        //"taskmgr.exe",
+    ];
 
     public App(string[] args)
     {
@@ -27,6 +40,12 @@ internal class App : BackgroundService
                     {
                         using (process)
                         {
+                            var commandLine = GetCommandLine(process);
+                            if (commandLine != null && _appLaunchersBlacklist.Any(e => commandLine.Contains(e, StringComparison.OrdinalIgnoreCase)))
+                            {
+                                continue;
+                            }
+
                             if ((_args.Length > 0 && _args[0] == "all") || process.StartTime > programStartTime)
                             {
                                 process.ProcessorAffinity = (nint)4294966527;
@@ -41,5 +60,23 @@ internal class App : BackgroundService
             });
             await Task.Delay(5000);
         }
+    }
+
+    private static string? GetCommandLine(Process process)
+    {
+        try
+        {
+            using ManagementObjectSearcher managementObjectSearcher = new($"SELECT CommandLine FROM Win32_Process WHERE ProcessId = {process.Id}");
+            foreach (var managementObject in managementObjectSearcher.Get())
+            {
+                return managementObject["CommandLine"]?.ToString();
+            }
+        }
+        catch
+        {
+            // ignore
+        }
+
+        return null;
     }
 }
